4e917fe5c9bf1ac4eee4f33d487cadf4291029e5
GRJesus <rod.giu@hotmail.com>
2019-09-03 15:47:41 -0300

    Carrinho de compras

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function addToCart (product) {
+function addToCart(product) {
   var cart = JSON.parse(sessionStorage.getItem("cart"));
   if (!cart) {
     cart = [];
@@ -16,14 +16,31 @@ $(document).ready(function () {
         price: productPrice
       });
   });
-
+  var productname = ("Produto = ");
+  var productprice = ("Preço = R$ ");
   var cart = sessionStorage.getItem("cart");
   if (cart !== null) {
     cart = JSON.parse(cart);
-    console.info("Carrinho 1 produto", cart[0].name);
-    console.info("Carrinho 1 produto", cart[1].name);
-    for (var i = 0; i < cart.lenght ; i++) {
-      $("#result").html(cart[i].name + "</br>");
+    //console.info("Carrinho 1 produto", cart[0].name);
+    //console.info("Carrinho 1 produto", cart[0].price);
+
+    for (var i = 0; i < cart.length; i++) {
+      $("#test").html(i);
+      $("#result").append('<div>' + productname + cart[i].name + ' ' + 
+      productprice + cart[i].price + "</div>" + 
+      '<button class="btn btn-primary" onClick="removeToCart(' + i + ')" "id="reset' + i + '" type="reset">Excluir Produto</button>'
+      + "</br>")
+    }    
   }
+})
+
+
+function removeToCart(value) {
+  var cart = sessionStorage.getItem("cart");
+  cart = JSON.parse(cart);
+  var removeItem = cart[value];
+  var newcart = [];
+  newcart = cart.splice($.inArray(removeItem, cart), 1);
+  location.reload();
+  return sessionStorage.setItem("cart", JSON.stringify(cart));
 }
-});
